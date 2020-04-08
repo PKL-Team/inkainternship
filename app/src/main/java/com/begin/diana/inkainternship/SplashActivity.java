a8eaaed4e2fe6ac573712076ca46559f0d6a9752
MARPC\Mardiana <now101246@gmail.com>
2020-04-08 18:56:26 +0700

    splash + menu

--- app/src/main/java/com/begin/diana/inkainternship/SplashActivity.java
+++ app/src/main/java/com/begin/diana/inkainternship/SplashActivity.java
@@ -2,13 +2,26 @@ package com.begin.diana.inkainternship;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
+    private static int splashInterval = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-    }
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent home = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(home);
+                finish();
+            }
+        }, splashInterval);
+    };
+
 }
