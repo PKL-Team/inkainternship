1fef814d6aa68d986983a4d075263bc356d44a8d
MARPC\Mardiana <now101246@gmail.com>
2020-04-18 14:57:16 +0700

    connect firebase, proses daftar dan login sukses (pakek methode createusingemailandpassword)

--- app/src/main/java/com/begin/diana/inkainternship/DaftarActivity.java
+++ app/src/main/java/com/begin/diana/inkainternship/DaftarActivity.java
@@ -1,41 +1,108 @@
 package com.begin.diana.inkainternship;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.PopupMenu;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class DaftarActivity extends AppCompatActivity {
 
     Button button;
+    Spinner list;
+    EditText txtEmail, txtPass1, txtPass2;
+    String sItem, sEmail, sPass1, sPass2;
+    FirebaseAuth mAuth;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daftar);
 
-        final Spinner List = findViewById(R.id.listItem);
+        list = findViewById(R.id.listItemDaftar);
         button = findViewById(R.id.btnRegister);
+        txtEmail = findViewById(R.id.txtEmailDaftar);
+        txtPass1 = findViewById(R.id.txtPass1);
+        txtPass2 = findViewById(R.id.txtPass2);
+        progressBar = findViewById(R.id.progressBar);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        PopupMenu dropDownMenu = new PopupMenu(getApplicationContext(), list);
+        dropDownMenu.getMenuInflater().inflate(R.menu.dropdown_menu, dropDownMenu.getMenu());
+        dropDownMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                Toast.makeText(getApplicationContext(), "You have clicked " + menuItem.getTitle(), Toast.LENGTH_LONG).show();
+                return true;
+            }
+        });
+
+        if (mAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(), Main2Activity.class));
+            finish();
+        }
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                PopupMenu dropDownMenu = new PopupMenu(getApplicationContext(), button);
-                dropDownMenu.getMenuInflater().inflate(R.menu.dropdown_menu, dropDownMenu.getMenu());
-                dropDownMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                sEmail = txtEmail.getText().toString().trim();
+                sPass1 = txtPass1.getText().toString().trim();
+                sPass2 = txtPass2.getText().toString().trim();
 
+                if (TextUtils.isEmpty(sEmail)){
+                    txtEmail.setError("Email belum diisi");
+                    return;
+                }
+                if (TextUtils.isEmpty(sPass1)){
+                    txtPass1.setError("Password belum diisi");
+                    return;
+                }
+                if (TextUtils.isEmpty(sPass2)){
+                    txtPass2.setError("Re-Password belum diisi");
+                    return;
+                }
+                if (sPass1.length() < 6 | sPass2.length() < 6){
+                    txtPass1.setError("Password harus lebih dari 6 karakter");
+                    txtPass2.setError("Password harus lebih dari 6 karakter");
+                    return;
+                }
+//                if (sPass1 != sPass2){
+//                    txtPass2.setError("Re-Password tidak sama dengan Password");
+//                    return;
+//                }
+                sItem = list.getSelectedItem().toString();
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                mAuth.createUserWithEmailAndPassword(sEmail, sPass1).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
-                    public boolean onMenuItemClick(MenuItem menuItem) {
-                        Toast.makeText(getApplicationContext(), "You have clicked " + menuItem.getTitle(), Toast.LENGTH_LONG).show();
-                        return true;
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(DaftarActivity.this, "Pendaftaran Akun Berhasil", Toast.LENGTH_SHORT).show();
+                            finish();
+                            startActivity(new Intent(getApplicationContext(), Main2Activity.class));
+                        }else {
+                            Toast.makeText(DaftarActivity.this, "Error" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                        }
                     }
                 });
-                dropDownMenu.show();
             }
         });
 
